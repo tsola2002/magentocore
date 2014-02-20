f5fc7c3e9e7d87e38a4f1c1ce53870ff5ebdbf6e
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2014-02-20 15:08:10 +0000

    02_02_ setting up the folder structure for a module and cofig necessary xml files

---
+++ app/code/local/Mmnamespace/Mmmodule/Helper/Data.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * app/code/local/MasteringMagento/Example/Helper/Data.php
+ *
+ * This example code is provided for use with the Mastering Magento video
+ * series, by Packt Publishing.
+ *
+ * @author    Franklin P. Strube <franklin.strube@gmail.com>
+ * @category  MasteringMagento
+ * @package   Example
+ * @copyright Copyright (c) 2012 Packt Publishing (http://packtpub.com)
+ */
+class MasteringMagento_Example_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    // Empty helper class
+}
