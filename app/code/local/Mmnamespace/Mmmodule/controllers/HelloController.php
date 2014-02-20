0b7990e3677b5ed08c26f3645c372fe2fec0c7db
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2014-02-20 15:52:54 +0000

    02_03_ creating a frontend controller within our module

---
+++ app/code/local/Mmnamespace/Mmmodule/controllers/HelloController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *
+ * Package: magentocore
+ * Filename: HelloController.php
+ * Author: solidstunna101
+ * Date: 20/02/14
+ * Time: 15:30
+ *
+ */
+
+class Mmnamespace_Mmmodule_HelloController extends Mage_Core_Controller_Front_Action
+{
+    public function worldAction()
+    {
+        echo 'Hello, World!';
+    }
+}
---
+++ app/code/local/Mmnamespace/Mmmodule/controllers/indexController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *
+ * Package: magentocore
+ * Filename: indexController.php
+ * Author: solidstunna101
+ * Date: 20/02/14
+ * Time: 15:30
+ *
+ */
+
+class Mmnamespace_Mmmodule_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+        echo 'Hello, World!, I am frm the index controller';
+    }
+}
